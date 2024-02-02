fcc11a664c9ce56aeef48feb66cadcc29b67ce45
o-master-o <kasian.m.w@gmail.com>
2024-02-02 19:58:04 +0100

    simplify locals, move colors to variables

--- logger.sh
+++ logger.sh
@@ -1,60 +1,51 @@
 #!/bin/bash
 
-# Logger script for sourcing in user scripts
-
-# Global variables
 LOG_DIR=""
 LOG_FILE=""
 
-# Function to setup logging
-setup_logging() {
-    local script_name script_dir timestamp
+# Colors
+GREEN="\e[0;32"
+ORANGE="\e[0;33"
+RED="\e[0;31"
+BLUE="\e[1;34"
+NC="\e[0m"
 
-    script_name=$(basename "$0")
-    script_dir=$(dirname "$0")
-    timestamp=$(date +"%Y_%m_%d__%H_%M_%S")
+setup_logging() {
+    local script_name=$(basename "$0")
+    local script_dir=$(dirname "$0")
+    local timestamp=$(date +"%Y_%m_%d__%H_%M_%S")
     LOG_DIR="${script_dir}/logging"
 
     mkdir -p "${LOG_DIR}" || return 1
     LOG_FILE="${LOG_DIR}/${script_name}_${timestamp}.log"
 }
 
-# Function to log messages
 log_message() {
-    local level color msg timestamp
-
-    level="$1"
-    color="$2"
-    msg="$3"
-    timestamp=$(date +"%Y-%m-%d %H:%M:%S")
+    local level="$1"
+    local color="$2"
+    local msg="$3"
+    local timestamp=$(date +"%Y-%m-%d %H:%M:%S")
 
     # Log to console with color
-    printf "\e[${color}m${timestamp} [${level}]\e[0m ${msg}\n"
+    printf "${color}m${timestamp} [${level}]${NC} ${msg}\n"
+
 
     # Log to file without color
     printf "${timestamp} [${level}] ${msg}\n" >> "${LOG_FILE}"
 }
 
-# Wrapper functions for different log levels
 log_info() {
-    log_message "INFO" "0;32" "$1"
+    log_message "INF" "$GREEN" "$1"
 }
 
 log_warn() {
-    log_message "WARN" "0;33" "$1"
+    log_message "WRN" "$ORANGE" "$1"
 }
 
 log_error() {
-    log_message "ERROR" "0;31" "$1"
+    log_message "ERR" "$RED" "$1"
 }
 
 log_header() {
-    log_message "HEADER" "1;34" "$1"
+    log_message "TAG" "$BLUE" "$1"
 }
-
-# Example usage
-# setup_logging
-# log_info "This is an info message."
-# log_warn "This is a warning message."
-# log_error "This is an error message."
-# log_header "This is a header message."
