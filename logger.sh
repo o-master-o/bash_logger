0da4ab25df2cede2f17f78a5b8bbb7e10a7f6678
o-master-o <kasian.m.w@gmail.com>
2024-02-02 19:19:43 +0100

    remove from writing to logfile colors

--- logger.sh
+++ logger.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 
+# Logger script for sourcing in user scripts
+
+# Global variables
 LOG_DIR=""
 LOG_FILE=""
 
+# Function to setup logging
 setup_logging() {
     local script_name script_dir timestamp
 
@@ -13,37 +17,44 @@ setup_logging() {
 
     mkdir -p "${LOG_DIR}" || return 1
     LOG_FILE="${LOG_DIR}/${script_name}_${timestamp}.log"
-
-    exec 3>&1 4>&2
-    trap 'exec 2>&4 1>&3' 0 1 2 3
-    exec 1>>"${LOG_FILE}" 2>&1
 }
 
-log_info() {
-    local msg timestamp
+# Function to log messages
+log_message() {
+    local level color msg timestamp
+
+    level="$1"
+    color="$2"
+    msg="$3"
     timestamp=$(date +"%Y-%m-%d %H:%M:%S")
-    msg="$1"
-    printf "\e[0;32m%s [INF]\e[0m %s\n" "${timestamp}" "${msg}" | tee /dev/fd/3
+
+    # Log to console with color
+    printf "\e[${color}m%s [%s]\e[0m %s\n" "${timestamp}" "${level}" "${msg}"
+
+    # Log to file without color
+    printf "%s [%s] %s\n" "${timestamp}" "${level}" "${msg}" >> "${LOG_FILE}"
+}
+
+# Wrapper functions for different log levels
+log_info() {
+    log_message "INFO" "0;32" "$1"
 }
 
 log_warn() {
-    local msg timestamp
-    timestamp=$(date +"%Y-%m-%d %H:%M:%S")
-    msg="$1"
-    printf "\e[0;33m%s [WRN]\e[0m %s\n" "${timestamp}" "${msg}" | tee /dev/fd/3
+    log_message "WARN" "0;33" "$1"
 }
 
 log_error() {
-    local msg timestamp
-    timestamp=$(date +"%Y-%m-%d %H:%M:%S")
-    msg="$1"
-    printf "\e[0;31m%s [ERR]\e[0m %s\n" "${timestamp}" "${msg}" | tee /dev/fd/3 >&2
+    log_message "ERROR" "0;31" "$1"
 }
 
 log_header() {
-    local msg timestamp
-    timestamp=$(date +"%Y-%m-%d %H:%M:%S")
-    msg="$1"
-    printf "\e[1;34m%s [HEADER]\e[0m %s\n" "${timestamp}" "${msg}" | tee /dev/fd/3
+    log_message "HEADER" "1;34" "$1"
 }
 
+# Example usage
+# setup_logging
+# log_info "This is an info message."
+# log_warn "This is a warning message."
+# log_error "This is an error message."
+# log_header "This is a header message."
